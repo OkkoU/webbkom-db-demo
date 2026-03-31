a12fe955e11a791634f729c08d602b9d38ff8182
Okko <okko.ukkola@gmail.com>
2026-03-31 16:29:39 +0300

    add ip address

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Morjens!", "v": "0.2" }
+    return { "msg": "Morjens Dev Okko!", "v": "0.2" }
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/api/ip")
+def ip(request: Request):
+    return {"ip":  request.client.host}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>IP-adressen är: {request.client.host}</h1>"
