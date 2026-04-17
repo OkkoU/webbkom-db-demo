f1a76a8f3f7f8aecb1debba36d97c08dd7e02df1
Okko <okko.ukkola@gmail.com>
2026-04-17 15:59:57 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import date
+from app.db import get_conn, create_schema
 
 app = FastAPI()
 
@@ -14,40 +17,115 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Skapa databas-schema
+create_schema()
+
+
+# Datamodell för bokning
+class Booking(BaseModel):
+    guest_id: int
+    room_id:  int
+    datefrom: date
+    dateto: date
+    addinfo: str | None = None
+
+
 @app.get("/")
 def read_root():
-    return { "msg": "Morjens Dev Okko!", "v": "0.2" }
+    # Testa databasen
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                'Databasen funkar!' AS msg,
+                version() as version
+        """)
+
+        db_status = cur.fetchone()
+
+    return { "msg": "Hotel-API", "db": db_status }
 
 
-@app.get("/api/ip")
-def ip(request: Request):
-    return {"ip":  request.client.host}
 
-
-@app.get("/ip", response_class=HTMLResponse)
-def ip(request: Request):
-    return f"<h1>IP-adressen är: {request.client.host}</h1>"
-
-
-# Hotel Rooms
-rooms = [
-    {
-        "room_number": 101,
-        "type": "Single",
-        "price": 80
-    },
-    {
-        "room_number": 202,
-        "type": "Double",
-        "price": 120
-    },
-    {
-        "room_number": 303,
-        "type": "Suite",
-        "price": 250
-    }
-]
-
+# --- Hotel Rooms --------------------
 @app.get("/rooms")
 def get_rooms():
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                id,
+                room_number,
+                type,
+                price
+            FROM hotel_rooms
+            ORDER BY room_number
+        """)
+
+        rooms = cur.fetchall()
+
     return rooms
+
+
+
+# --- Get one Room --------------------
+@app.get("/rooms/{id}")
+def get_room(id: int):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT *
+            FROM hotel_rooms
+            WHERE id = %s
+        """, [id])
+
+        room = cur.fetchone()
+
+    return room
+
+
+
+# --- Bookings --------------------
+@app.post("/bookings")
+def create_booking(booking: Booking):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO hotel_bookings (
+                guest_id,
+                room_id,
+                datefrom,
+                dateto,
+                addinfo
+            )
+            VALUES (
+                %s, %s, %s, %s, %s
+            ) RETURNING id
+        """, (
+            booking.guest_id,
+            booking.room_id,
+            booking.datefrom,
+            booking.dateto,
+            booking.addinfo
+        ))
+
+        new_booking = cur.fetchone()
+
+    return {"msg": "Booking created!", "id": new_booking['id']}
+
+
+@app.get("/bookings")
+def get_bookings():
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                b.id,
+                b.datefrom,
+                b.dateto,
+                b.addinfo,
+                r.room_number
+            FROM hotel_bookings b
+            JOIN hotel_rooms r ON b.room_id = r.id
+            ORDER BY b.datefrom
+        """)
+
+        bookings = cur.fetchall()
+
+    return bookings
